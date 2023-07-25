373855d1417203a63ec12a128df49ae0c94ba559
phunghx2 <phunghx2@fpt.com.vn>
2023-07-25 04:39:49 +0000

    assignment 4 part 1

--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -1,5 +1,7 @@
 #include <stdbool.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
  * This structure should be dynamically allocated and passed as
@@ -19,6 +21,9 @@ struct thread_data{
      * Set to true if the thread completed with success, false
      * if an error occurred.
      */
+    pthread_mutex_t *mutex;
+    int wait_to_obtain_ms;
+    int wait_to_release_ms;
     bool thread_complete_success;
 };
 
